646ec7c2207c1c5ddb001203f1647ce0ef0641e2
Ron Cooke <ron.cooke@me.com>
2014-01-24 20:56:12 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  context 'Home page' do
+    it "should have the content 'Microblog'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Microblog')
+    end
+
+    it "should have the title 'Microblog'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Microblog')
+    end
+  end
+
+  context 'Help page' do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
+  end
+
+  context 'About page' do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'about'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
+  end
+
+end
